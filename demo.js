b6c1d39663df77449377321f51788f00cd63fb39
ShivamParihar <xavieranand23@gmail.com>
2019-04-20 16:04:01 +0530

    Uploading starting files

---
+++ demo.js
@@ -0,0 +1,15 @@
+
+
+var input1 = document.querySelector(".input1");
+var input2 = document.querySelector(".input2");
+var body = document.querySelector("body");
+var print = document.querySelector("h3");
+
+function changeBackground(){
+	var back = "linear-gradient(to right,"+input1.value+","+input2.value+")";
+	body.style.background = back;
+	print.innerHTML = back;
+}
+
+input1.addEventListener("input",changeBackground);
+input2.addEventListener("input",changeBackground);
